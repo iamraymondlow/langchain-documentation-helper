0da74ee6522c7e190c6c7c3ba5e3e464291ee987
Raymond Low <iamraymondlow@gmail.com>
2024-01-16 18:18:08 +0000

    Implemented RetrievalQA chain

--- ingestion.py
+++ ingestion.py
@@ -3,6 +3,7 @@ from langchain.document_loaders import ReadTheDocsLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Pinecone
+from consts import INDEX_NAME
 import pinecone
 
 pinecone.init(
@@ -28,9 +29,7 @@ def ingest_docs() -> None:
 
     print(f"Going to insert {len(documents)} to Pinecone")
     embeddings = OpenAIEmbeddings()
-    Pinecone.from_documents(
-        documents, embeddings, index_name="langchain-doc-index"
-    )
+    Pinecone.from_documents(documents, embeddings, index_name=INDEX_NAME)
     print("****** Added to Pinecone vectorstore vectors")
 
 
